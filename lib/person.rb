d7cb90d2d8aa91b6dc1c0e1eda7faded9d56c867
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-11-17 13:25:59 -0800

    Created a Dog and Person Class

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def job=(job_type)
+        @job = job_type
+    end
+
+    def job
+        @job
+    end
+end
